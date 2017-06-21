8f60441be8418351f614479d022663711d4f8c5f
Flk427 <flk427@gmail.com>
2017-06-21 17:31:05 +0400

    [*] model...

--- src/guiMode/ui/FilesDecisionListView/FilesDecisionModel.cpp
+++ src/guiMode/ui/FilesDecisionListView/FilesDecisionModel.cpp
@@ -18,21 +18,32 @@ void FilesDecisionModel::setFilesInfo(const CloneHunter::FilesInfo& filesInfo)
 }
 
 
-//QModelIndex FilesDecisionModel::index(int row, int column, const QModelIndex& parent) const
-//{
-//}
+QModelIndex FilesDecisionModel::index(int row, int column, const QModelIndex& parent) const
+{
+}
 
 //QModelIndex FilesDecisionModel::parent(const QModelIndex& child) const
 //{
 //}
 
-//int FilesDecisionModel::rowCount(const QModelIndex& parent) const
-//{
-//}
+int FilesDecisionModel::rowCount(const QModelIndex& parent) const
+{
+	if (parent.isValid())
+	{
+//		return ???
+	}
+	else
+	{
+		return m_filesDecisionFiles.size();
+	}
 
-//int FilesDecisionModel::columnCount(const QModelIndex& parent) const
-//{
-//}
+	return 0;
+}
+
+int FilesDecisionModel::columnCount(const QModelIndex& parent) const
+{
+	return 1;
+}
 
 //QVariant FilesDecisionModel::data(const QModelIndex& index, int role) const
 //{
--- src/guiMode/ui/FilesDecisionListView/FilesDecisionModel.h
+++ src/guiMode/ui/FilesDecisionListView/FilesDecisionModel.h
@@ -16,10 +16,10 @@ public:
 
 	// QAbstractItemModel interface
 public:
-//	virtual QModelIndex index(int row, int column, const QModelIndex& parent) const;
+	virtual QModelIndex index(int row, int column, const QModelIndex& parent) const;
 //	virtual QModelIndex parent(const QModelIndex& child) const;
-//	virtual int rowCount(const QModelIndex& parent) const;
-//	virtual int columnCount(const QModelIndex& parent) const;
+	virtual int rowCount(const QModelIndex& parent) const;
+	virtual int columnCount(const QModelIndex& parent) const;
 //	virtual QVariant data(const QModelIndex& index, int role) const;
 //	virtual bool setData(const QModelIndex& index, const QVariant& value, int role);
 //	virtual Qt::ItemFlags flags(const QModelIndex& index) const;
